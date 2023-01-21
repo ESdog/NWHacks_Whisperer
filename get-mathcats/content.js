65468d0f471063a0cfa9459794dbb122900199be
beep <beepmo@students.cs.ubc.ca>
2023-01-21 12:38:27 -0800

    make get-mathcats test

---
+++ get-mathcats/content.js
@@ -0,0 +1,7 @@
+let images = document.getElementsByTagName('img');
+
+for (let i = 0, l = images.length; i < l; i++) {
+    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
+
+let math = MathJax.startup.document.getMathItemsWithin(document.body);
