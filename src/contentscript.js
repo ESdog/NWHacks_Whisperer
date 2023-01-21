09b80c599f766b72d301e780058f17786269d49e
Charity <charity.grey1@gmail.com>
2023-01-21 14:55:38 -0800

    Added action documents inside manifest.json

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,9 +1,9 @@
 
+window.onload = function() {getAllMathItems()};
+
 // returns array of all math jax objects
 const getAllMathItems = () => {
     const mathItems = window.MathJax.Hub.getAllJax();
     console.log(mathItems);
     return mathItems;
 }
-
-window.onload = getAllMathItems();
