884a90a345acfadc1fba961c2dd4d185b3fd305e
beep <beepmo@students.cs.ubc.ca>
2023-01-21 15:40:56 -0800

    delete background.js

--- get-mathcats/content.js
+++ get-mathcats/content.js
@@ -1,9 +1,9 @@
 alert('first alert')
 
 let mathitems = window.MathJax.Hub.getAllJax();
+alert(mathitems.length)
 
 for (let i = 0, l = mathitems.length; i < l; i++) {
-    // paragraphs[i].src = 'http://placekitten.com/' + paragraphs[i].width + '/' + paragraphs[i].height;
     console.log('found math item');
     alert('found math item')
 }
--- src/background.js
+++
@@ -1,3 +0,0 @@
-chrome.tabs.onUpdated.addListener(
-    callback: alert(`Hello World`),
-);
