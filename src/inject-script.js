34f9b94f4735436eea14287ae6737a61900ecfa7
beep <beepmo@students.cs.ubc.ca>
2023-01-22 09:33:47 -0800

    v3 function

--- src/inject-script.js
+++ src/inject-script.js
@@ -1,23 +1,5 @@
 console.log('<----- Injected script started running ----->');
 
-/*
-// return json type object {"MathJax": MathJax DOM object}
-function checkMathJax() {
-    let main = {};
-   // solution to "window.MathJax is not a function" — it is just an attribute
-    main.MathJax = window.MathJax;
-
-    // this breakpoint works!
-    alert(`MathJax Version:` + main.MathJax.version);
-    return main;
-}
-
-let mathjax = checkMathJax();
-// postMessage is not able to construct objects with methods, thus, flatten mathjax into string
-mathjax = JSON.parse(JSON.stringify(mathjax));
-window.postMessage({ type: "FROM_PAGE", essential: mathjax });
-*/
-
 // return json type object {"MathItems": array of MathItem object}
 const getAllMathItems = () => {
 
@@ -26,7 +8,7 @@ const getAllMathItems = () => {
     if (mathJaxObj && mathJaxObj.version[0] == '2') {
         talkToMathJaxV2();
     } else if (mathJaxObj && mathJaxObj.version[0] == '3') {
-        main.mathItems =  mathJaxObj.Hub.getAllJax();
+        talkToMathJaxV3();
     } else { // no mathjax
         main.mathItems = [];
         console.log("MathJax and image math not found on site");
