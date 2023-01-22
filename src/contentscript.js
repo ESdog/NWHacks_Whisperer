8f8d823e5654f5591316d5d04a9a65504e953455
beep <beepmo@students.cs.ubc.ca>
2023-01-21 19:24:35 -0800

    update contentscript.js with contents of content.js from get-catmath

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,19 +1,37 @@
+alert(`Content script should be working`)
+// alert is used as breakpoint
 
+console.log('<----- Content script started running ----->');
 
-window.onload = function() {
-    getAllMathItems();
-};
-
-/*
-const injectMathScript = () => {
-
-    const headNode = document.getElementsByTagName("head")[0];
-    const newScriptNode = document.createElement("script");
-    newScriptNode.setAttribute("type", "text/javascript");
-    newScriptNode.setAttribute("type", "mathScript.js");
-    headNode.appendChild(newScriptNode);
+function injectScript(file_path, tag) {
+    // inject our script at top of body
+    var node = document.getElementsByTagName(tag)[0];
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', file_path);
+    node.appendChild(script);
 }
-*/
+
+injectScript(chrome.runtime.getURL('inject-script.js'), 'body');
+
+console.log('<----- Inject script injected into DOM ----->');
+
+window.addEventListener("message", function (event) {
+    // only accept messages from the current tab
+    if (event.source != window)
+        return;
+
+    // remove condition (chrome.app.isInstalled)
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        chrome.runtime.sendMessage({ essential: event.data.essential });
+    }
+}, false);
+
+
+
+
+
+//========== ignire below
 
 
 // returns array of all math jax objects
