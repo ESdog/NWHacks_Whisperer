595437e265d261def01ae0717e60a471c10b9e6f
Alec Currie <e9a8i@students.cs.ubc.ca>
2023-01-22 10:54:38 -0800

    Continued work on regex search.

--- src/whisperUI.js
+++ src/whisperUI.js
@@ -1,4 +1,5 @@
-import {getRegexOccurences} from "./regex-search";
+//import {getRegexOccurrences} from "/Whisper/src/regex-search.js";
+
 
 const search = document.getElementById("searchText");
 const latexBtn = document.getElementById("LatexBtn");
@@ -8,7 +9,7 @@ const regexBtn = document.getElementById("RegexBtn");
 let state = false;
 
 search.addEventListener("keyup", function () {
-    getRegexOccurences(search.value);
+    getRegexOccurrences(search.value);
 });
 
 latexBtn.addEventListener("click", function () {
@@ -24,3 +25,28 @@ regexBtn.addEventListener("click", function () {
 });
 
 latexBtn.click();
+
+function getRegexOccurrences(expression) {
+    //alert("called RegEx highlight")
+    const regex = new RegExp(expression, 'gi');
+
+    findTextInNode(window.body, expression);
+}
+
+function findTextInNode (parentNode, expression) {
+    for(let i = parentNode.childNodes.length-1; i >= 0; i--){
+        let node = parentNode.childNodes[i];
+
+        //  Make sure this is a text node
+        if(node.nodeType == Element.TEXT_NODE){
+            let text = node.textContent;
+            text = text.replace(/(<span style="background-color:red>|<\/span>)/gim, '');
+
+            const newText = text.replace(expression,'$1<span style="background-color:red;">$2</span>');
+            node.textContent = newText;
+        } else if(node.nodeType == Element.ELEMENT_NODE){
+            //  Check this node's child nodes for text nodes to act on
+            findTextInNode(node, expression);
+        }
+    }
+}
