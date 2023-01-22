55bca71ef302dbd1a08566618f78c3232f9829de
beep <beepmo@students.cs.ubc.ca>
2023-01-21 22:22:01 -0800

    delete background.js and its mention in manifest.json

--- src/background.js
+++
@@ -1,7 +0,0 @@
-window.perfWatch = {};
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    alert(`onMesage got called from background`);
-    window.perfWatch[sender.tab.id] = message.essential || null;
-    alert(`tab id got set from background`)
-});
