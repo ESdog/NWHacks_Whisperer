ec4e1b81dbaefc1137b826e71247359554c5589a
Alec Currie <e9a8i@students.cs.ubc.ca>
2023-01-21 20:55:37 -0800

    Fixed merge disaster (hopefully)

--- src/whisperUI.js
+++ src/whisperUI.js
@@ -1,6 +1,23 @@
 const search = document.getElementById("searchText");
-const button = document.getElementById("searchImg");
+const latexBtn = document.getElementById("LatexBtn");
+const regexBtn = document.getElementById("RegexBtn");
+const pressedColour = "rgb(89,89,89)";
+const unpressedColour = "rgb(241,241,241)";
+
+// If false, search latex, else search regex
+let state = false;
 
 search.addEventListener("keyup", function () {
-    console.log("hello");
+    alert(search.value);
+});
+latexBtn.addEventListener("click", function () {
+    latexBtn.style.backgroundColor = pressedColour;
+    regexBtn.style.backgroundColor = unpressedColour;
+    state = false;
+});
+
+regexBtn.addEventListener("click", function () {
+    regexBtn.style.backgroundColor = pressedColour;
+    latexBtn.style.backgroundColor = unpressedColour;
+    state = true;
 });
