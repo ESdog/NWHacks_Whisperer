59d453067869498a8588bf3f18d3f89f98477ea5
Charity <charity.grey1@gmail.com>
2023-01-21 22:45:26 -0800

    added removeBackslash fn part 1

--- src/contentscript.js
+++ src/contentscript.js
@@ -38,6 +38,7 @@ window.addEventListener("message", (event)=>{
         alert('Message received by contentscript');
         ids = event.data.essential;
         tex = event.data.essential.tex;
+        scanItemsForMatch(userInputString, ids, tex);
     }
 });
 
@@ -47,12 +48,11 @@ window.addEventListener("message", (event)=>{
 
 // ERROR: does not like backslashes
 //returns a match of all the latex that matches userInputStr
-const scanItemsForMatch = (userInputString) => {
-    const matchedElements = getAllMathItems().filter((mathItem) => {
+const scanItemsForMatch = (userInputString, ids, tex) => {
+    const matchedElements = tex().map((latexString) => {
         userInputString = userInputString;
-        const itemLatex = mathItem.originalText;
 
-        return itemLatex.includes(userInputString);
+        return latexString.includes(userInputString);
     });
 
     highlightElements(matchedElements);
@@ -67,5 +67,14 @@ const highlightElements = (matchedElements) => {
     }
 }
 
-
+//removes backslashes that are escaped, ie `\\`
+const removeBackslash = (textString) => {
+    let newString = "";
+    for (let i =0; i<textString.length; i++) {
+        if (textString[i] != "\\") {
+            newString += textString[i];
+        }
+    }
+    return newString;
+}
 
