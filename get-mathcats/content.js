e11514380340e44e2bcc4c6eef6c9ddb26b12d97
beep <beepmo@students.cs.ubc.ca>
2023-01-21 17:51:10 -0800

    remove condition (chrome.app.isInstalled)

--- get-mathcats/content.js
+++ get-mathcats/content.js
@@ -21,7 +21,8 @@ window.addEventListener("message", function (event) {
     if (event.source != window)
         return;
 
-    if (event.data.type && (event.data.type == "FROM_PAGE") && typeof chrome.app.isInstalled !== 'undefined') {
+    // remove condition (chrome.app.isInstalled)
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
         chrome.runtime.sendMessage({ essential: event.data.essential });
     }
 }, false);
