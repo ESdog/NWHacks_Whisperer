d84fc82c59b169ac8786ef01b8a21890d501c027
beep <beepmo@students.cs.ubc.ca>
2023-01-21 16:38:58 -0800

    updated content.js

--- get-mathcats/content.js
+++ get-mathcats/content.js
@@ -1,10 +1,22 @@
-alert('first alert')
+console.log('<----- Content script started running ----->');
 
-let mathitems = window.MathJax.Hub.getAllJax();
-alert(mathitems.length)
-
-for (let i = 0, l = mathitems.length; i < l; i++) {
-    console.log('found math item');
-    alert('found math item')
+function injectScript(file_path, tag) {
+    // inject our script at top of body
+    var node = document.getElementsByTagName(tag)[0];
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', file_path);
+    node.appendChild(script);
 }
 
+injectScript(chrome.extension.getURL('inject-script.js'), 'body');
+
+window.addEventListener("message", function (event) {
+    // only accept messages from the current tab
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE") && typeof chrome.app.isInstalled !== 'undefined') {
+        chrome.runtime.sendMessage({ essential: event.data.essential });
+    }
+}, false);
--- get-mathcats/inject-script.js
+++ get-mathcats/inject-script.js
@@ -1,9 +1,10 @@
 console.log('<----- Injected script started running ----->');
 
+// return json type object {"MathJax": MathJax DOM object}
 function checkMathJax() {
-    let main = [];
+    let main = {};
 
-    main = window.MathJax();
+    main.MathJax = window.MathJax();
 
     return main;
 }
@@ -12,3 +13,27 @@ setInterval(() => {
     let mathjax = checkMathJax();
     window.postMessage({ type: "FROM_PAGE", essential: mathjax });
 }, 500);
+
+
+
+// console.log('<----- Content script started running ----->');
+//
+// function injectScript(file_path, tag) {
+//     var node = document.getElementsByTagName(tag)[0];
+//     var script = document.createElement('script');
+//     script.setAttribute('type', 'text/javascript');
+//     script.setAttribute('src', file_path);
+//     node.appendChild(script);
+// }
+//
+// injectScript(chrome.extension.getURL('inject-script.js'), 'body');
+//
+// window.addEventListener("message", function (event) {
+//     // only accept messages from the current tab
+//     if (event.source != window)
+//         return;
+//
+//     if (event.data.type && (event.data.type == "FROM_PAGE") && typeof chrome.app.isInstalled !== 'undefined') {
+//         chrome.runtime.sendMessage({ essential: event.data.essential });
+//     }
+// }, false);
