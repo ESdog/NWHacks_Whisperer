2c1fa9a4af4ae2e40841c6c8ec9d3c6c7fdc3140
Charity <charity.grey1@gmail.com>
2023-01-22 09:16:45 -0800

    Tested scan utillity functions in isolation

--- get-mathcats/inject-script.js
+++ get-mathcats/inject-script.js
@@ -15,3 +15,77 @@ let mathjax = checkMathJax();
 // postMessage is not able to construct objects with methods, thus, flatten mathjax into string
 mathjax = JSON.parse(JSON.stringify(mathjax));
 window.postMessage({ type: "FROM_PAGE", essential: mathjax });
+
+
+// ========== utility functions below
+const ESCAPE_CHARACTERS = {
+    "\b": "b",
+    "\f": "f",
+    "\n": "n",
+    "\r": "r",
+    "\t": "t",
+    "\v": "v"
+  }
+  
+  
+  //If "\u" and "\x" are in the strings, not supposed to be possible options, remained
+  //INVARIANT: ids and tex are tandem worklists and the same length
+  //returns a match of all the latex that matches userInputStr
+  const scanItemsForMatch = (userInputString, ids, tex) => {
+    let matchedElementIds = [];
+    for (let i = 0; i < ids.length; i++) {
+      latexString = removeDoubleBackslash(tex[i]);
+      userInputString = removeSingleBackslash(userInputString);
+  
+      if (latexString.includes(userInputString)) {
+        matchedElementIds.push(ids[i]);
+      }
+    }
+  
+    highlightElements(matchedElementIds);
+    return matchedElementIds;
+  }
+  
+  //@param matchedElementsIds is a list of String
+  const highlightElements = (matchedElementIds) => {
+    for (let i = 0; i < matchedElementIds.length; i++) {
+      const locationOfElem = document.getElementById(matchedElementIds[i]);
+      const parentElem = locationOfElem.parentElement;
+      if (parentElem.tagName != 'p') {
+        locationOfElem.parentElement.style.backgroundColor = "red"; //for now
+      } else {
+        
+      }
+    }
+  }
+  
+  
+  //returns the same string except without escaped backslashes 
+  //ie:""\\frac\n" returns "frac\n"
+  const removeDoubleBackslash = (texString) => {
+    let newString = "";
+    for (let i = 0; i < texString.length; i++) {
+      if (texString[i] != "\\") {
+        newString += texString[i];
+      }
+    }
+    return newString;
+  }
+  
+  //returns the same string except without escaped backslashes 
+  //ie:"\frac\\n" returns "frac\\n"
+  const removeSingleBackslash = (texString) => {
+    const escapedKeys = Object.keys(ESCAPE_CHARACTERS)
+    let newString = "";
+    for (let i = 0; i < texString.length; i++) {
+      const charToParse = texString[i];
+      if (escapedKeys.includes(charToParse)) {
+        newString += ESCAPE_CHARACTERS[charToParse];
+      } else {
+        newString += charToParse;
+      }
+    }
+    return newString;
+  }
+  
+  
