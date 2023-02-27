b91e774a5312a64300df829861bea10f80ae9887
beep <beepmo@students.cs.ubc.ca>
2023-02-26 16:54:56 -0800

    works if port manually created in console using same code

--- src/whisperUI.js
+++ src/whisperUI.js
@@ -26,15 +26,8 @@ var tabId;
 chrome.tabs.query({active: true, currentWindow: true}, function (tabArray) {
     tabId = tabArray[0].id
 });
-console.log("Tab id: " + tabId);
+console.log("Tab id:", tabId);
 
-var port = chrome.tabs.connect(tabId, {name: "knockknock"});
+var port = chrome.tabs.connect(tabId);
 console.log("var port as seen by whisperUI: ",port);
-port.postMessage({joke: "Knock knock"});
-port.onMessage.addListener(function(msg) {
-    console.log(msg.question);
-    if (msg.question === "Who's there?")
-        port.postMessage({answer: "Madame"});
-    else if (msg.question === "Madame who?")
-        port.postMessage({answer: "Madame... Bovary"});
-});
+port.postMessage({mymsg: "Knock knock"});
