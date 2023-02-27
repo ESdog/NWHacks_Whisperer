eccf569e81eb72dbe6e6d70c9bfc57a4079f5980
beep <beepmo@students.cs.ubc.ca>
2023-02-26 16:21:52 -0800

    to supply tab Id to tabs.connect

--- src/contentscript.js
+++ src/contentscript.js
@@ -19,6 +19,21 @@ window.addEventListener("message", (event) => {
     console.log("contentscript received: " + event.data);
 })
 
+console.log("chrome.tabs API as seen by contentscript: "+chrome.tabs)
+/* Receive search value from whisperUI
+ */
+chrome.runtime.onConnect.addListener(function(port) {
+    console.assert(port.name === "knockknock");
+    port.onMessage.addListener(function(msg) {
+        console.log(msg.joke);
+        if (msg.joke === "Knock knock")
+            port.postMessage({question: "Who's there?"});
+        else if (msg.answer === "Madame")
+            port.postMessage({question: "Madame who?"});
+        else if (msg.answer === "Madame... Bovary")
+            port.postMessage({question: "I don't get it."});
+    });
+});
 
 
 
--- src/whisperUI.js
+++ src/whisperUI.js
@@ -4,11 +4,9 @@ window.addEventListener("message", (event) => {
     console.log("whisperUI received: " + event.data);
 })
 
-/* Post user search as message on keyup
-   @warning Works in extension window but an extraneous Uncaught TypeError is thrown in webpage console
+/* Post user search as window message on keyup
  */
 const search = window.document.getElementById("searchText");
-// @warning Extension console displays correct log; webpage console displays null
 console.log("element to attach keyup listener to:" + search);
 search.addEventListener("keyup", function () {
     if(search.value == "") {
@@ -17,3 +15,32 @@ search.addEventListener("keyup", function () {
     window.postMessage(search.value,"*");
 });
 
+console.log("chrome.tabs API as seen by whisperUI: ",chrome.tabs)
+
+/* Post user search as chrome message on keyup
+ */
+
+/* Open connection by providing tabID
+ */
+function getCurrentTabId(cb) {
+    var query = {active: true, currentWindow: true};
+    chrome.tabs.query(query, function (tabArray) {
+        console.log("Tab id: " + tabArray[0].id);
+        cb(tabArray[0].id)
+    });
+}
+function connectToCurrentTab () {
+    getCurrentTabId(function(currentTabId) {
+        return chrome.tabs.connect(currentTabId, {name: "popup"});
+    });
+}
+var port = connectToCurrentTab();
+console.log("var port as seen by whisperUI: ",port)
+port.postMessage({joke: "Knock knock"});
+port.onMessage.addListener(function(msg) {
+    console.log(msg.question);
+    if (msg.question === "Who's there?")
+        port.postMessage({answer: "Madame"});
+    else if (msg.question === "Madame who?")
+        port.postMessage({answer: "Madame... Bovary"});
+});
