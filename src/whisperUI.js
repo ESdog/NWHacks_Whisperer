d91958520867cd12ebe35363a4cf0213f41579a2
beep <beepmo@students.cs.ubc.ca>
2023-02-26 17:09:03 -0800

    removed background from manifest.json

--- src/whisperUI.js
+++ src/whisperUI.js
@@ -1,22 +1,3 @@
-window.postMessage("hello from whisperUI","*");
-console.log('<----- whisper: bare-bones inject-script said hello ----->');
-window.addEventListener("message", (event) => {
-    console.log("whisperUI received: " + event.data);
-})
-
-/* Post user search as window message on keyup
- */
-const search = window.document.getElementById("searchText");
-console.log("element to attach keyup listener to:" + search);
-search.addEventListener("keyup", function () {
-    if(search.value == "") {
-        return;
-    }
-    window.postMessage(search.value,"*");
-});
-
-console.log("chrome.tabs API as seen by whisperUI: ", chrome.tabs)
-
 /* Post user search as chrome message on keyup
  */
 
