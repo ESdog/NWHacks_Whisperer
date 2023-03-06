4b4f2354cdae45fa491349178234ce3d12188f18
Alec Currie <e9a8i@students.cs.ubc.ca>
2023-03-05 16:18:44 -0800

    Updated whisperUI.js to send message to port.

--- src/whisperUI.js
+++ src/whisperUI.js
@@ -1,5 +1,12 @@
 /* Send message from whisperUI to contentscript
  */
+var port;
+const search = document.getElementById("searchText");
+search.addEventListener("keyup", function () {
+    console.log(search.value);
+    port.postMessage({mymsg: search.value})
+});
+
 chrome.tabs.query({active: true, currentWindow: true}, function (tabArray) {
     /*
     chrome.tabs.query is used to get the tabId, which is required in chrome.tabs.connect(tabId).
@@ -8,10 +15,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function (tabArray) {
     var tabId = tabArray[0].id;
     console.log("whisperUI tab id:", tabId);
 
-    /*
-
-     */
-    var port = chrome.tabs.connect(tabId);
+    port = chrome.tabs.connect(tabId);
     port.postMessage({mymsg: "hello hello from whisperUI through chrome.runtime.port!"});
     console.log('<----- sent message from whisperUI to contentscript via chrome.runtime.port ----->');
 });
